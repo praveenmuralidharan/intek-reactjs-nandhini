8c42f029b0f4b9b67b41a2c75522839ae8f7d9c0
Nandhini <nandhini1171992@gmail.com>
2022-01-23 09:15:43 +0530

    learned mapping

--- src/App.js
+++ src/App.js
@@ -1,11 +1,48 @@
-import './App.css';
-
+import React from 'react';
+  
 function App() {
+  
+ const students = [
+              {
+                'id': 1, 
+                'name': 'Praveen', 
+                'Gender': 'Male'
+              },
+              {
+                'id': 2, 
+                'name': 'Nandhini', 
+                'Gender': 'Female'
+              },
+              {
+                'id': 3, 
+                'name': 'Pavi', 
+                'Gender': 'Female'
+              },
+          ];
+  
   return (
-    <div className="text-center">
-      <h1>Hello Mark</h1>
+    <div className="container">
+        
+   
+        <table className="table table-bordered">
+            <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Email</th>
+            </tr>
+  
+            {students.map((student, index) => (
+              <tr data-index={index}>
+                <td>{student.id}</td>
+                <td>{student.name}</td>
+                <td>{student.Gender}</td>
+              </tr>
+            ))}
+  
+        </table>
+  
     </div>
   );
 }
-
+  
 export default App;
