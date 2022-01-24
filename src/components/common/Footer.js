0f7571f012353ee1731ef6fcc35ae44e92c4fec7
praveen.pravin6 <praveen.pravin6@gmail.com>
2022-01-24 07:52:44 +0530

    Layout added

---
+++ src/components/common/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react'; 
+
+const Footer = () => {  
+  return (<div>
+        By Intek
+    </div>
+  );
+}
+  
+export { Footer };
--- src/index.js
+++ src/index.js
@@ -4,18 +4,16 @@ import { BrowserRouter as Router , Switch , Route } from 'react-router-dom';
 import './index.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import { Homepage , Userdetails } from './App';
+import { Layout } from './components/Layout';
 
 ReactDOM.render(
-  <Router>
-    <Switch>
-      <Route exact path="/" component={Homepage} />
-      <Route exact path="/userdetails/:student_id" component={Userdetails} />
-    </Switch>
-  </Router>,
+  <Layout>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Homepage} />
+        <Route exact path="/userdetails/:student_id" component={Userdetails} />
+      </Switch>
+    </Router>
+  </Layout>,
   document.getElementById('root')
 );
-
-
-{/* <React.StrictMode>
-    <Userdetails />
-  </React.StrictMode>, */}
